68c16f48a7b2026ca37cf812245d6a1b57fcd403
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-13 12:30:36 +0100

    using prev project to sort my users app

---
+++ users/serializers.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from . models import Profile
+from django.contrib.auth.models import User
+
+from . utils import SendMail
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','email']
+
+class ProfileSerailizer(serializers.ModelSerializer):
+    class Meta:
+        model = UserSerializer()
+        fields = ['fullname','gender','phone','profile_pix']
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+    username = serializers.CharField(write_only=True)
+    email = serializers.EmailField(write_only=True)
+    class Meta:
+        model =Profile
+        fields=['fullname','username','email','password1','password2','gender','phone','profile_pix']
+
+    def validate(self,data):
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError('Password does not match')
+        if not data['fullname']:
+            raise serializers.ValidationError('Fullname must be fill')
+        return data
+    
+    def create(self,validated_data):
+        username = validated_data.pop('username')
+        email = validated_data.pop('email')
+        password = validated_data.pop('password1')
+
+        user = User.objects.create_user(username=username,email=email,password=password)
+
+        profile = Profile.objects.create(
+            user = user,
+            fullname =validated_data['fullname'],
+            phone =validated_data['phone'],
+            gender =validated_data['gender'],
+            profile_pix =validated_data.get('profile_pix')
+        )
+        SendMail(email)
+        return profile
+
+
+class UpdateProfileSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(source='user.username',required=False)
+    email = serializers.EmailField(source='user.email',required=False)
+    class Meta:
+        model =Profile
+        fields=['fullname','username','email','gender','phone','profile_pix']
+    def update(self,instance,validated_data):
+        user_data = validated_data.pop('user',{})
+        user = instance.user
+        if 'username' in user_data:
+            user.username = user_data['username']
+        if 'email' in user_data:
+            user.email = user_data['email']
+        user.save()
+ 
+        instance.fullname = validated_data.get('fullname')
+        instance.gender = validated_data.get('gender')
+        instance.phone = validated_data.get('phone')
+        instance.profile_pix = validated_data.get('profile_pix')
+        instance.save()
+    
+        return instance
+
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
+from . serializers import UserSerializer,UpdateProfileSerializer,RegistrationSerializer
+from . models import Profile
+from django.contrib.auth.models import User
 
-# Create your views here.
+from django.contrib.auth import authenticate, login,logout
+
+# registration
+class RegistrationView(APIView):
+    def get(self,request):
+        if request.user.is_authenticated:
+            return redirect('dashboard')
+    def post(self,request):
+        try:
+            if request.user.is_authenticated:
+                return Response({"Message":"You are logged in already"})
+            
+            serializer = RegistrationSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+
+# Login
+class LoginView(APIView):
+    def post(self,request):
+        try:
+            username = request.data.get('username')
+            password = request.data.get('password')
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return Response({"Message":"User login successfully!"}, status=status.HTTP_200_OK)
+            return Response({"Message":"username/password not correct"}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+# Logout
+class LogoutView(APIView):
+    def post(self,request):
+        try:
+            logout(request)
+            return Response({"Message":"Logout Successful!"},status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+
+# dashboard
+@permission_classes([IsAuthenticated])
+class DashboardView(APIView):
+    def get(self,request):
+        try:
+            user = request.user.profile
+            return Response({"Message": "welcome" +" "+ user.fullname})
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+
+# update
+class UpdateProfileView(APIView):
+    def get(self,request):
+        try:
+            profile = request.user.profile
+            serializer = UpdateProfileSerializer(profile)
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+    def put(self,request):
+        try:
+            profile = request.user.profile
+            serializer = UpdateProfileSerializer(profile,data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+        
